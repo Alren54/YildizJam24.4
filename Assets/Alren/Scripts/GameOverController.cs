a06086c0aa2f9d256a9c88be323e294a5641ad25
Alren54 <hollyali1234@hotmail.com>
2024-04-28 18:36:50 +0300

    gameover screen added

--- Assets/Alren/Scripts/GameOverController.cs
+++ Assets/Alren/Scripts/GameOverController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,9 +9,19 @@ public class GameOverController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> buildings = new();
     [SerializeField] private GameObject GameOverPanel;
+    [SerializeField] private TextMeshProUGUI endGameTime;
+    private float timer = 0;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
     public void CheckIfAllBuildingsAlive()
     {
         GameOverPanel.SetActive(true);
+        StringBuilder str = new();
+        str.Append("You lived for " + (int)timer + " seconds");
+        endGameTime.SetText(str.ToString());
         Time.timeScale = 0f;
     }
 
